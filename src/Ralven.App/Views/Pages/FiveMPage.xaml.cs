4ac110a0c805bed7bdb443a88a3163814b47a35b
marquezini <felipemarquesini10@gmail.com>
2026-09-08 16:33:29 -0300

    feat(games): adiciona acesso oficial ao ReShade

--- src/Ralven.App/Views/Pages/FiveMPage.xaml.cs
+++ src/Ralven.App/Views/Pages/FiveMPage.xaml.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using System.Windows;
+using Ralven.App.Services;
 using Ralven.Contracts;
 using UserControl = System.Windows.Controls.UserControl;
 
@@ -6,6 +8,8 @@ namespace Ralven.App.Views.Pages;
 
 public partial class FiveMPage : UserControl
 {
+    private const string ReShadeDownloadUrl = "https://reshade.me/#download";
+
     public FiveMPage() => InitializeComponent();
 
     private MainWindow? Shell => Window.GetWindow(this) as MainWindow;
@@ -19,4 +23,10 @@ public partial class FiveMPage : UserControl
         Shell?.RequestNavigateToOptimizer(OptimizationScope.GeneralWindows);
 
     private void OpenHistory_Click(object sender, RoutedEventArgs e) => Shell?.RequestNavigateToHistory();
+
+    private void InstallReShade_Click(object sender, RoutedEventArgs e) =>
+        ExternalLauncher.TryOpen(() => Process.Start(new ProcessStartInfo(ReShadeDownloadUrl)
+        {
+            UseShellExecute = true
+        }));
 }
